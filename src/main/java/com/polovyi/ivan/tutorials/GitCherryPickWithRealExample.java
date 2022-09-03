1b8b1c17f2157b9d70b49c122b7d60a40d461bf6
Ivan Polovyi <ivan.polovyi@sensedia.com>
2022-09-03 13:53:50 -0300

    First production feature

---
+++ src/main/java/com/polovyi/ivan/tutorials/GitCherryPickWithRealExample.java
@@ -0,0 +1,14 @@
+package com.polovyi.ivan.tutorials;
+
+public class GitCherryPickWithRealExample {
+
+    public static void main(String[] args) {
+
+        methodDeployedInProduction();
+
+    }
+
+    private static void methodDeployedInProduction() {
+        System.out.println("Production code!");
+    }
+}
